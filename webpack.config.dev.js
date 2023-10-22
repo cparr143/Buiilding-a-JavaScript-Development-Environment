9b2a878287dd6c0f0ed7db2d751c43abe44b06a9
Cierra Parr <cierra.parr@yahoo.com>
2023-10-22 07:21:28 -0400

    module 9 work in progres

---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+// Webpack.Config.dev.js
+
+import path from "path";
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+};
+
